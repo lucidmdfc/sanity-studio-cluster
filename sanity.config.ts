f939d3324f56440e7c95bad78199bd07e12601c8
youssef-mous <106970977+youssef-mous@users.noreply.github.com>
2024-10-11 17:14:41 +0100

    add sanity media plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
-
+import {media} from 'sanity-plugin-media'
 export default defineConfig({
   name: 'default',
   title: 'cluster portal',
@@ -10,7 +10,22 @@ export default defineConfig({
   projectId: '6l74mhm8',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool(),
+    visionTool(),
+    media({
+      creditLine: {
+        enabled: true,
+        // boolean - enables an optional "Credit Line" field in the plugin.
+        // Used to store credits e.g. photographer, licence information
+        excludeSources: ['unsplash'],
+        // string | string[] - when used with 3rd party asset sources, you may
+        // wish to prevent users overwriting the creditLine based on the `source.name`
+      },
+      maximumUploadSize: 10000000,
+      // number - maximum file size (in bytes) that can be uploaded through the plugin interface
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
